3056ef25cad0f8ee559485348839f09deb23b854
Shepilova <magic.hollow@yandex.kz>
2025-03-10 18:30:21 +0400

    ТЗ-14. checkstyle.

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -5,10 +5,14 @@ import ru.practicum.shareit.item.model.Item;
 import java.util.List;
 
 public interface ItemRepository {
-    List<Item> getItemsList(long userId);
-    Item getItemById(long id);
-    Item addNewItem(Item item);
-    Item updateItem(Item item);
-    List<Item> searchItems(String text, Long userId);
 
+    List<Item> getItemsList(long userId);
+
+    Item getItemById(long id);
+
+    Item addNewItem(Item item);
+
+    Item updateItem(Item item);
+
+    List<Item> searchItems(String text, Long userId);
 }
--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -3,9 +3,14 @@ package ru.practicum.shareit.user;
 import java.util.List;
 
 public interface UserRepository {
+
     List<User> getAllUsersList();
+
     User getUserById(long id);
+
     User addUser(User user);
+
     User updateUser(User user);
+
     void deleteUserById(long id);
 }
